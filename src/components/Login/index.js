8a73a2eb7cba41aaa7ba8835041386bdf08bc427
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-14 10:28:48 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,30 +6,31 @@ import Cookies from 'js-cookie'
 class Login extends Component {
   success = jwtToken => {
     const {history} = this.props
-    Cookies.set('jwt-token', jwtToken, {expires: 30})
-    history.replace('/')
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/home')
   }
 
   login = async () => {
+    const userDetails = {username: 'rahul', password: 'rahul@2021'}
     const api = 'https://apis.ccbp.in/login'
     const opt = {
       method: 'POST',
+      body: JSON.stringify(userDetails),
     }
-
     const get = await fetch(api, opt)
-    console.log(get)
+
     const res = await get.json()
-    console.log(res)
-     if (get.ok === true) {
+
+    if (get.ok === true) {
       this.success(res.jwt_token)
     }
   }
 
   render() {
-    const jwt = Cookies.get('jwt-token')
+    const jwt = Cookies.get('jwt_token')
 
     if (jwt !== undefined) {
-      return <Redirect to="/" />
+      return <Redirect to="/home" />
     }
     return (
       <div className="div1">
