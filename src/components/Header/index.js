a7731f128321479d80f6998f6c176fb97f028e9c
Gova2000 <138764817+Gova2000@users.noreply.github.com>
2023-09-14 10:28:05 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,12 @@ import './index.css'
 
 const Header = () => (
   <div className="div">
-    <Link to="/home">Home</Link>
-    <Link to="/about">About</Link>
+    <Link to="/home" className="l">
+      Home
+    </Link>
+    <Link to="/about" className="l">
+      About
+    </Link>
   </div>
 )
 
